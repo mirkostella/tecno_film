22a1102a5f5c9fb330f2dc8b8d06f995fce70e85
soniafranco <sonia.franco2205@gmail.com>
2022-03-17 11:38:50 +0100

    aggiunti file mancanti

---
+++ php/gestore_film.php
@@ -0,0 +1,160 @@
+<?php
+
+class GestoreFilm{
+    public $id=null;
+    public $titolo;
+    public $trama;
+    public $durata;
+    public $dataUscita;
+    public $prezzoA;
+    public $prezzoN;
+    public $copertina;
+    public $descrizione;
+    public $genere;
+    
+    public function __construct($array){
+        
+        $this->titolo=$array['titolo'];
+        $this->trama=$array['trama'];
+        $this->durata=$array['durata'];
+        $this->dataUscita=$array['dataUscita'];
+        $this->prezzoA=$array['prezzoA'];
+        $this->prezzoN=$array['prezzoN'];
+        $this->copertina=$array['copertina'];
+        $this->descrizione=$array['descrizione'];
+        $this->genere=$array['generi'];
+    
+    }
+
+    public function controlloPresenzaFilm(){
+        //controllo per titolo del film
+        $queryTuttiIFilm="SELECT film.titolo as titolo FROM film";
+        $connessione=new Connessione();
+        $connessione->apriConnessione();
+        $films=$connessione->interrogaDB($queryTuttiIFilm);
+        $connessione->chiudiConnessione();
+        $presenza=false;
+        foreach($films as $valore){
+            if($this->titolo==$valore['titolo'])
+                $presenza=true;
+        }
+        return $presenza;
+    }
+
+    public function getErrori(){
+        $errori=array(
+            'errTitolo'=>"",
+            'errCopertina'=>"",
+            'errAlt'=>"",
+            'errTrama'=>"",
+            'errDurata'=>"",
+            'errNuovoGenere'=>"",
+            'errData'=>"",
+            'errPrezzoA'=>"",
+            'errPrezzoN'=>""
+        );
+        //possibili errori:
+        //film giá presente
+        //prezzo noleggio maggiore o uguale del prezzo di acquisto
+        //campo dati assente
+    }
+
+    public function recuperaFilePath(){
+        $file_path='';
+        $connessione=new Connessione();
+        $connessione->apriConnessione();
+        $gestisci_img = new gestione_img();
+        if(isset($_FILES['copertinaFilm']) && is_uploaded_file($_FILES['copertinaFilm']['tmp_name'])){
+            $upload_result=$gestisci_img->uploadImg("img_film/", "copertinaFilm");
+            print_r($upload_result['path']);
+            if($upload_result['error']==''){
+                $file_path=$upload_result['path'];
+                return $file_path;
+            }
+            else
+                return false;
+        }
+    }
+
+    public function recuperaIDGeneri(){
+        $ris=array();
+        $connessione=new Connessione();
+        $connessione->apriConnessione();
+        foreach($this->genere as $valore){
+            $queryIDGenere="SELECT ID FROM genere WHERE nome_genere='".$valore."'";
+            $IDGenere=$connessione->interrogaDB($queryIDGenere);
+            array_push($ris,$IDGenere[0]['ID']);
+        }
+        $connessione->chiudiConnessione();
+        return $ris;
+    }
+
+    public function inserisciFilm(){
+        $path=$this->recuperaFilePath();
+        $queryFotoCopertina="INSERT INTO foto_film (path, descrizione) VALUES 
+        (\"$path\", '".$_POST['descrizione']."')";
+
+        $connessione=new Connessione();
+        $connessione->apriConnessione();
+        $connessione->inizioTransazione();
+
+        $esitoFoto=$connessione->eseguiQuery($queryFotoCopertina);
+        if(!$esitoFoto)
+            echo 'esito foto fallito';
+
+        $queryIDFoto="SELECT id FROM foto_film ORDER BY id DESC LIMIT 1";
+        $IDFoto=$connessione->interrogaDB($queryIDFoto);
+        
+        print_r($this->titolo);
+        print_r($IDFoto[0]['id']);
+        print_r($this->trama);
+        print_r($this->durata);
+        print_r($this->dataUscita);
+        print_r($this->prezzoA);
+        print_r($this->prezzoN);
+        $time = strtotime($this->dataUscita);
+        $newformat = date('d-m-Y',$time); 
+        echo $newformat;
+        $queryFilm="INSERT INTO film (titolo,copertina,trama,durata,data_uscita,prezzo_acquisto,prezzo_noleggio) VALUES ('".$this->titolo."',".$IDFoto[0]['id'].",'".$this->trama."',".$this->durata.",".$newformat.",".$this->prezzoA.",".$this->prezzoN.")";
+        
+        $ok=true;
+        $esitoFilm=$connessione->eseguiQuery($queryFilm);
+        if(!$esitoFilm){
+            $ok=false;
+            echo 'primo';
+        }
+             
+        if($esitoFilm){
+            //recupero l'id del film
+            $queryIDFilm="SELECT ID FROM film ORDER BY id DESC LIMIT 1";
+            $queryIDGenere="SELECT ID FROM genere WHERE nome_genere='azione'";
+            $genere=$connessione->interrogaDB($queryIDGenere)[0]['ID'];
+            $this->id=$connessione->interrogaDB($queryIDFilm)[0]['ID'];
+            $x=$this->recuperaIDGeneri();
+            foreach($x as $valore){
+                $queryAppartenenza="INSERT INTO appartenenza (ID_film,ID_genere) VALUES 
+                ($this->id,".$valore.")";
+                $esitoAppartenenza=$connessione->eseguiQuery($queryAppartenenza);
+                if(!$esitoAppartenenza)
+                    $ok=false;
+           }
+        
+        $connessione->fineTransazione($ok);      
+        $connessione->chiudiConnessione();
+        if(!$ok){
+            $this->id=null;
+            echo 'inserimento fallito';
+        }
+        if($ok)
+            echo 'inserito con successo';
+            
+        return $ok;
+    
+        }
+    }
+//fine classe GestoreFilm
+}
+
+
+
+?>
---
+++ php/ins_film_admin.php
@@ -0,0 +1,65 @@
+<?php
+require_once("sessione.php");
+require_once("connessione.php");
+require_once("gestore_film.php");
+require_once("upload_img.php");
+
+print_r($_POST);
+$pagina=file_get_contents("../html/ins_film_admin.html");
+//se sono arrivato alla pagina cercando di inserire un film
+if(isset($_POST['inserisciFilm'])){
+
+    $datiNuovoFilm=array(     
+        'titolo'=>$_POST['titoloFilm'],
+        'trama'=>$_POST['tramaFilm'],
+        'durata'=>$_POST['durataFilm'],
+        'dataUscita'=>$_POST['dataUscitaFilm'],
+        'prezzoA'=>$_POST['prezzoAcquistoFilm'],
+        'prezzoN'=>$_POST['prezzoNoleggioFilm'],
+        'copertina'=>"",
+        'descrizione'=>$_POST['descrizione'],
+        'generi'=>$_POST['generi']
+    );
+
+    $gestore=new GestoreFilm($datiNuovoFilm);
+    //NON inserisco e ricarico la pagina con i messaggi d'errore
+    //mantenendo i campi delle form
+    // if($gestore->getErrori()){
+
+    // }
+    // else{
+        
+    //     if(!$gestore->inserisciFilm()){
+            
+    //     }
+    // }
+    $successo=$gestore->inserisciFilm();
+    
+}
+else{
+    $queryNomiGeneri="SELECT DISTINCT nome_genere FROM genere";
+    $connessione=new Connessione();
+    $connessione->apriConnessione();
+    $generi=$connessione->interrogaDB($queryNomiGeneri);
+    $listaGeneri="";
+    foreach($generi as $valore){
+        $nuovaVoce='<label for="'.$valore['nome_genere'].'">'.$valore['nome_genere'].'</label>
+        <input type="checkbox" id="'.$valore['nome_genere'].'" name="generi[]" value="'.$valore['nome_genere'].'" class="checkmark">';
+        
+        $listaGeneri=$listaGeneri.$nuovaVoce;
+    }
+    
+    $pagina=str_replace('%listaGeneri%',$listaGeneri,$pagina);
+    $pagina=str_replace("%errTitolo%","",$pagina);
+    $pagina=str_replace("%errCopertina%","",$pagina);
+    $pagina=str_replace("%errAlt%","",$pagina);
+    $pagina=str_replace("%errTrama%","",$pagina);
+    $pagina=str_replace("%errDurata%","",$pagina);
+    $pagina=str_replace("%errNuovoGenere%","",$pagina);
+    $pagina=str_replace("%errData%","",$pagina);
+    $pagina=str_replace("%errPrezzoA%","",$pagina);
+    $pagina=str_replace("%errPrezzoN%","",$pagina);
+
+}
+    echo $pagina;
+?>
