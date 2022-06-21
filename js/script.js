e6d83f85c17dfede6d77cf15f9165120d9c76b20
mirkostella <mirko.stella@studenti.unipd.it>
2022-06-21 10:20:15 +0200

    commit prima di iniziare con il file js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//controllo formato email
+function controlloEmail(email){}
+//controllo formato password
+function controlloPassword(password){}
+//controllo data maggiorenne
+function controlloEta(data){}
