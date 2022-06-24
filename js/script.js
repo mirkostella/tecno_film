bd6c9f297bd1ae62422eaf760ddc616e4d18fff0
mirkostella <mirko.stella@studenti.unipd.it>
2022-06-24 14:49:00 +0200

    commit prima di js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-//controllo formato email
-function controlloEmail(email){}
-//controllo formato password
-function controlloPassword(password){}
-//controllo data maggiorenne
-function controlloEta(data){}
+
+//ritorna l'array con l'email trovata altrimenti null
+function controlloEmail(email){
+    return new RegExp(/^(([^{}<>()\[\]\.,;:\s@\"\'\+#&\*$]+(\.[^{}<>()\[\]\.,;:\s@\"\'\+#&\*$]+)*))@(([^{}<>()[\]\.,;:\s@\"\'\+#&\*$]+\.)+(it|com|net|org))$/).exec(email);
+}
+//ritorna vero se la password é valida altrimenti falso
+function controlloPassword(password){
+    return new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/).test(password);
+}
+
+
+ 
+window.onload=function(){   
+    alert("ciao");
+}
