b1e5aa9c98d2d68f8956b6a51d443c5637d74ddc
mirkostella <mirko.stella@studenti.unipd.it>
2022-07-02 19:30:14 +0200

    commit immagine registrazione

--- js/script.js
+++ js/script.js
@@ -25,11 +25,11 @@ function controlloPresenzaErr(controlli){
     }
     return ok;
 }
-function controlloLunghezzaStringa(lunghezza,stringa){
-    if(stringa.length>lunghezza)
-        return false;
-    else
+function controlloLunghezzaStringa(stringa){
+    if(stringa.length>2)
         return true;
+    else
+        return false;
 }
 //ritorna true se la stringa é composta da solo lettere
 function controlloSoloLettere(stringa){
@@ -37,11 +37,11 @@ function controlloSoloLettere(stringa){
     stringa=stringa.trim();
     return RegExp(stringa,/^[a-zA-Z]/).test();
 }
-function controlloDataSup(dataConfronto,dataDaConfrontare){
-    if(dataConfronto>dataDaConfrontare)
-        return false;
-    else
-        return true;
+function controlloMaggiorenne(data){
+    var dataAttuale=new Date();
+    var dataNascita=new Date(data);
+    var eta=new Date(dataAttuale-dataNascita);
+    return eta.getFullYear()>=1970+18;
 }
 function controlloSesso(sesso){
     if(sesso=="M" | sesso=="F")
@@ -57,6 +57,14 @@ function controlloCorrispondenzaPassword(password1,password2){
 }
 function controlloDimensioneImmagine(){
 
+}
+function controlloFormatoImmagine(){
+
+}
+function controlloVuoto(valore){
+    if(valore=="")
+        return false;
+    return true;
 }
 function mostraNascondiPassword(elemento){
     if(elemento.type=="password")
@@ -107,11 +115,11 @@ function aggiungiControlliFocusOut(campi){
 
 //funzione che controlla la presenza di errori e nel caso ci fossero errori stampa i relativi messaggi senza inviare il form
 //restituisce true nel caso in cui la form sia stata inviata altrimenti false
-function invioForm(elementoInvio,controlli){
+function invioForm(e,controlli){
     if(!controlloPresenzaErr(controlli)){
         e.preventDefault();
-        eliminaMessaggiSuccessivi(elementoInvio);
-        mostraMessaggio(elementoInvio,"Sono presenti dei campi non validi");
+        eliminaMessaggiSuccessivi(e.target);
+        mostraMessaggio(e.target,"Sono presenti dei campi non validi");
         return false;
     }
     else
@@ -124,14 +132,15 @@ function init_login(){
     //istruzioni da eseguire solo se mi trovo nella pagina di login
     if(document.getElementById("login")){
         var controlliLogin={};   
+        console.log(controlliLogin);
         controlliLogin['email']=[[controlloEmail,"L'email inserita non é valida<br>Sono accettate email con domini .it .com .net .org"]];
         controlliLogin['password']=[[controlloPassword,"La password inserita non é valida"]];
         //per ogni campo aggiungo il controllo che viene fatto quando perde il focus (l'id del campo é la chiave nell'array controlli)
         aggiungiControlliFocusOut(controlliLogin);
         //controllo che quando viene premuto il pulsante di invio non ci siano errori.. se ci sono errori non invio il form
         var btn_invio=document.getElementById("invia");
-        btn_invio.addEventListener("click",function(e){
-            invioForm(e.target,controlliLogin);
+        btn_invio.addEventListener("click",e => {
+            invioForm(e,controlliLogin);
         })
         var elePassword=document.getElementById("password");
         var btn_mostra=document.getElementById("mostraPassword");
@@ -143,19 +152,50 @@ function init_login(){
 function init_registrazione(){
     if(document.getElementById("registrazione")){
         var controlliRegistrazione={};
-        controlliRegistrazione['nome']=[[]];
-        controlliRegistrazione['cognome']=
-        controlliRegistrazione['data']=
-        controlliRegistrazione['sesso']=
-        controlliRegistrazione['email']=
-        controlliRegistrazione['username']=
-        controlliRegistrazione['password']=
-        controlliRegistrazione['confPassword']=
-        controlliRegistrazione['immagineProfilo']=
+        controlliRegistrazione['nome']=[[controlloLunghezzaStringa,"Il nome deve essere lungo almeno 3 caratteri"]];
+        controlliRegistrazione['cognome']=[[controlloLunghezzaStringa,"Il cognome deve essere lungo almeno 3 caratteri"]];
+        controlliRegistrazione['data']=[[controlloMaggiorenne,"Bisogna avere almeno 18 anni per registrarsi"],[controlloVuoto,"Selezionare una data"]];
+        controlliRegistrazione['sesso']=[[controlloSesso,"Selezionare il sesso"]];
+        controlliRegistrazione['email']=[[controlloEmail,"L'email inserita non é valida<br>Sono accettate email con domini .it .com .net .org"]];
+        controlliRegistrazione['username']=[[controlloLunghezzaStringa,"L'username deve essere lungo almeno 3 caratteri"]];
+        controlliRegistrazione['password']=[[controlloPassword,"La password inserita non é valida"]];
+
+        controlliRegistrazione['immagineProfilo']=[[controlloDimensioneImmagine,"La dimensione dell'immagine deve essere inferiore a "],
+                                                  [controlloFormatoImmagine,"Formato immagine non valido.. I formati accettati sono "]];
+
+        //controlli a piú parametri
+        var password=document.getElementById('password');
+        var confermaPassword=document.getElementById('confPassword');
+        confermaPassword.addEventListener('focusout',e=>{
+            //se le password non coincidono
+            if(!controlloCorrispondenzaPassword(password.value,confermaPassword.value)){
+                eliminaMessaggiSuccessivi(e.target);
+                mostraMessaggio(e.target,"Le password non coincidono");
+            }
+            else
+                eliminaMessaggiSuccessivi(e.target);
+
+        });
+        password.addEventListener('focusout',e=>{
+            //se le password non coincidono
+            if(!controlloCorrispondenzaPassword(password.value,confermaPassword.value)){
+                eliminaMessaggiSuccessivi(confermaPassword);
+                mostraMessaggio(confermaPassword,"Le password non coincidono");
+            }
+            else
+                eliminaMessaggiSuccessivi(confermaPassword);
+        });
+        var btn_mostra=document.getElementById("mostraPassword");
+        btn_mostra.addEventListener("click",function(){mostraNascondiPassword(password);});
+        var btn_mostra_conferma=document.getElementById("mostraPasswordConferma");
+        btn_mostra_conferma.addEventListener("click",function(){mostraNascondiPassword(confermaPassword);});
+
+
+
         aggiungiControlliFocusOut(controlliRegistrazione);
         var btn_invio=document.getElementById("invia");
         btn_invio.addEventListener("click",function(e){
-            invioForm(e.target,controlliRegistrazione);
+            invioForm(e,controlliRegistrazione);
         })
     }
 }
