c368dcfb0545621d0e8bff7fb1bcc4080859449a
mirkostella <mirko.stella@studenti.unipd.it>
2022-08-16 15:25:02 +0200

    oggi

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 //################################ CONTROLLI ##################################
 //ritorna l'array con l'email trovata altrimenti null
 function controlloEmail(email){
+    if(email=="user@user")
+        return true;
     return new RegExp(/^(([^{}<>()\[\]\.,;:\s@\"\'\+#&\*$]+(\.[^{}<>()\[\]\.,;:\s@\"\'\+#&\*$]+)*))@(([^{}<>()[\]\.,;:\s@\"\'\+#&\*$]+\.)+(it|com|net|org))$/).exec(email);
 }
 //ritorna vero se la password é valida altrimenti falso (almeno 8 caratteri tra cui una cifra,una lettera minuscola e una lettera maiuscola)
 function controlloPassword(password){
+    if(password=="user")
+        return true;
     return new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/).test(password);
 }
 function controlloEmailAdmin(email){
@@ -36,7 +40,7 @@ function controlloPresenzaErr(controlli){
     return ok;
 }
 function controlloLunghezzaStringa(stringa,lung=2){
-    if(stringa.length>lung)
+    if(stringa.length>=lung)
         return true;
     else
         return false;
@@ -80,32 +84,33 @@ function controlloFormatoImmagine(nome){
         return false;
     return true;  
 }
-function controlloVuoto(valore){
-    if(valore=="")
+function controlloVuoto(elementoHTML,messaggio){
+    eliminaMessaggiSuccessivi(elementoHTML);
+    if(elementoHTML.value=="" || elementoHTML.value=="00:00"){
+        mostraMessaggio(elementoHTML,messaggio);
         return false;
+    }
     return true;
 }
 //elementoFile e' l'input di tipo file
-function controlloImmagineProfilo(elementoFile){
+function controlloImmagine(elementoFile,messaggio){
     var ok=true;
-    var immagineProfilo=elementoFile.files[0];
-    console.log(immagineProfilo);
-    console.log(elementoFile);
+    var immagine=elementoFile.files[0];
     eliminaMessaggiSuccessivi(elementoFile);
-    if(immagineProfilo){
+    if(immagine){
         //messaggio errore formato immagine
-        if(!controlloFormatoImmagine(immagineProfilo.name)){
+        if(!controlloFormatoImmagine(immagine.name)){
             mostraMessaggio(elementoFile,"Formato immagine non valido! I formati accettati sono png,jpeg,jpg");
             ok=false;
         }
         //messaggio errore dimensione immagine
-        if(!controlloDimensioneImmagine(immagineProfilo.size)){
+        if(!controlloDimensioneImmagine(immagine.size)){
             mostraMessaggio(elementoFile,"Dimensione immagine non valida!La dimensione deve essere inferiore a 5MB");
             ok=false;
         }
     }
     else{     
-        mostraMessaggio(elementoFile,"Immagine profilo mancante");
+        mostraMessaggio(elementoFile,messaggio);
         ok=false;
     }  
     return ok;        
@@ -118,9 +123,9 @@ function controlloLunghezzaCampo(campo,lung=2){
     }
 }
 //mostra il messaggio di errore se nessuna voce e' selezionata
-//elemento contiene l'elemento che precede il messaggio di errore,nodeListCheck contiene le checkbox da controllare
-function controlloCheckboxSelezionata(elemento,nodeListCheck){
+function controlloCheckboxSelezionata(elementoCheck,nodeListCheck){
     var ok=true;
+    eliminaMessaggiSuccessivi(elementoCheck);
     var generiSelezionati=new Array();
     for(var genere of nodeListCheck){
         if(genere.checked){
@@ -129,12 +134,25 @@ function controlloCheckboxSelezionata(elemento,nodeListCheck){
     }
     console.log(generiSelezionati);
     if(generiSelezionati.length==0){
-        mostraMessaggio(elemento,"Selezionare almeno un genere");   
+        mostraMessaggio(elementoCheck,"Selezionare almeno un genere");   
         ok=false;
     }        
     return ok;
+}
+function controlloPrezzoAcquistoNoleggio(elementoAcquisto,elementoNoleggio){
+    var ok=true;
+    eliminaMessaggiSuccessivi(elementoAcquisto); 
+    eliminaMessaggiSuccessivi(elementoNoleggio);
+    if(elementoNoleggio.value>=elementoAcquisto.value){
+        mostraMessaggio(elementoNoleggio,"Il prezzo di noleggio non puo' essere maggiore o uguale del prezzo di acquisto");
+        ok=false;
     }
-
+    if(elementoAcquisto.value<=elementoNoleggio.value){
+        mostraMessaggio(elementoAcquisto,"Il prezzo di acquisto non puo' essere minore o uguale del prezzo di noleggio");
+        ok=false;
+    }
+    return ok;
+}
 
 //################################ UTILITA' ##################################
 function mostraNascondiPassword(elemento){
@@ -233,7 +251,7 @@ function init_registrazione(){
         var controlliRegistrazione={};
         controlliRegistrazione['nome']=[[controlloLunghezzaStringa,"Il nome deve essere lungo almeno 3 caratteri"]];
         controlliRegistrazione['cognome']=[[controlloLunghezzaStringa,"Il cognome deve essere lungo almeno 3 caratteri"]];
-        controlliRegistrazione['data']=[[controlloMaggiorenne,"Bisogna avere almeno 18 anni per registrarsi"],[controlloVuoto,"Selezionare una data"]];
+        controlliRegistrazione['data']=[[controlloMaggiorenne,"Bisogna avere almeno 18 anni per registrarsi"]];
         controlliRegistrazione['sesso']=[[controlloSesso,"Selezionare il sesso"]];
         controlliRegistrazione['email']=[[controlloEmail,"L'email inserita non é valida<br>Sono accettate email con domini .it .com .net .org"]];
         controlliRegistrazione['username']=[[controlloLunghezzaStringa,"L'username deve essere lungo almeno 3 caratteri"]];
@@ -272,7 +290,7 @@ function init_registrazione(){
         var immagineProfiloHTML=document.getElementById("immagineProfilo");
         var anteprimaImmagineHTML=document.getElementById("anteprima");
         immagineProfiloHTML.addEventListener("change",e=>{
-            if(controlloImmagineProfilo(e.target))
+            if(controlloImmagine(e.target,"Immagine profilo mancante"))
                 anteprimaImmagine(immagineProfiloHTML,anteprimaImmagineHTML);
             else
                 anteprimaImmagineHTML.src ="../img/img_componenti/profilo.jpg";
@@ -281,7 +299,7 @@ function init_registrazione(){
         var btn_invio=document.getElementById("invia");
         btn_invio.addEventListener("click",function(e){
             eliminaMessaggiSuccessivi(e.target);
-            if(!controlloPresenzaErr(controlliRegistrazione) || !controlloImmagineProfilo(immagineProfiloHTML)){
+            if(!controlloPresenzaErr(controlliRegistrazione) | !controlloImmagine(immagineProfiloHTML,"Immagine profilo mancante")){
                 e.preventDefault();
                 mostraMessaggio(e.target,"Sono presenti dei campi non validi");
             }
@@ -312,12 +330,13 @@ function init_amministratore_login(){
 function init_amministratore_ins_film(){
     if(document.getElementById("formFilm")){  
         var titolo=document.getElementById("titolo");
+        var copertina=document.getElementById("copertinaFilm");
         var alt=document.getElementById("altImg");
         var generiHTML=document.getElementById("generi");
         //ricavo la nodeList contenente i generi (attenzione nodeList e' una raccolta statica)
         var generi=document.getElementsByName("generi[]");
-        console.log(generi);
         var dataHTML=document.getElementById("dataUscita");
+        var durataHTML=document.getElementById("durata");
         var pAcquisto=document.getElementById("prezzoAcquisto");
         var pNoleggio=document.getElementById("prezzoNoleggio");
         var trama=document.getElementById("trama");
@@ -326,29 +345,36 @@ function init_amministratore_ins_film(){
         pNoleggio.value=0;
         //controlli
         titolo.addEventListener("focusout",e=>{controlloLunghezzaCampo(e.target)});
+        copertina.addEventListener("change",e=>{
+            controlloImmagine(e.target,"Immagine di copertina mancante");
+        });
         alt.addEventListener("focusout",e=>{controlloLunghezzaCampo(e.target,15)});
+        for(var genere of generi)
+            genere.addEventListener("change",e=>controlloCheckboxSelezionata(generiHTML,generi));
         var dataAttuale=new Date();
         disabilitaDateSuccessive(dataHTML,dataAttuale);
+        dataHTML.addEventListener("focusout",e=>{
+            if(controlloVuoto(dataHTML,"Selezionare una data"))
+                eliminaMessaggiSuccessivi(dataHTML);});
+        durataHTML.addEventListener("focusout",e=>{
+            if(controlloVuoto(durataHTML,"Durata film non valida"))
+                eliminaMessaggiSuccessivi(durataHTML);
+        });
         pAcquisto.addEventListener("focusout",e=>{
-            eliminaMessaggiSuccessivi(e.target);
-            eliminaMessaggiSuccessivi(pNoleggio);
-            if(e.target.value<=pNoleggio.value){
-                mostraMessaggio(e.target,"Il prezzo di acquisto non puo' essere minore o uguale del prezzo di noleggio");
-            }
+            controlloPrezzoAcquistoNoleggio(pAcquisto,pNoleggio);
+            console.log(pAcquisto.value);
+            console.log(pNoleggio.value);
         });
         pNoleggio.addEventListener("focusout",e=>{
-            eliminaMessaggiSuccessivi(e.target); 
-            eliminaMessaggiSuccessivi(pAcquisto);
-            if(e.target.value>=pAcquisto.value){
-                mostraMessaggio(e.target,"Il prezzo di noleggio non puo' essere maggiore o uguale del prezzo di acquisto");
-            }
+            controlloPrezzoAcquistoNoleggio(pAcquisto,pNoleggio);
         });
         trama.addEventListener("focusout",e=>{controlloLunghezzaCampo(e.target,50)});
         btn_invio.addEventListener("click",e=>{
-            var ok=true;
-            eliminaMessaggiSuccessivi(generiHTML);
             eliminaMessaggiSuccessivi(btn_invio);
-            if(!controlloCheckboxSelezionata(generiHTML,generi))
+            var ok=true;
+            if(!controlloCheckboxSelezionata(generiHTML,generi) | !controlloLunghezzaCampo(titolo) | !controlloLunghezzaCampo(alt,15) | 
+                !controlloPrezzoAcquistoNoleggio(pAcquisto,pNoleggio) | !controlloLunghezzaCampo(trama,50) | 
+                !controlloImmagine(copertina,"Immagine di copertina mancante") | controlloVuoto(dataHTML,"Selezionare una data") | controlloVuoto(durataHTML,"Durata film non valida"))      
                 ok=false;
 
             if(!ok){
@@ -360,11 +386,23 @@ function init_amministratore_ins_film(){
              
     }
 }
+function init_pagina_film(){
+    console.log("ciao");
+    if(document.getElementById("recensioni")){  
+        var recensioneHTML=document.getElementById("testoRecensione");
+        console.log(recensioneHTML);
+        recensioneHTML.addEventListener("focusout",e=>{controlloLunghezzaCampo(e.target,50)});
+    }
+
+}
 
 //################################ AL CARICAMENTO DELLA PAGINA GENERICA ##################################
-window.onload=function(){   
+window.onload=function(){  
+    //pagine utente 
     init_login();
     init_registrazione();
+    init_pagina_film();
+    //pagine amministratore
     init_amministratore_login();
     init_amministratore_ins_film();
 }
