06ebaeb0c3465e41a3e62b8c876c30c868621019
soniafranco <74913219+soniafranco@users.noreply.github.com>
2022-09-08 19:46:45 +0200

    sistemata relazione

--- php/sessione.php
+++ php/sessione.php
@@ -12,15 +12,4 @@
     if(!isset($_SESSION['admin'])){
         $_SESSION['admin']=false;
     }
-
-    if(isset($_SESSION['pagina_corrente'])){
-        if(basename($_SERVER['REQUEST_URI'])!=$_SESSION['pagina_corrente'])
-            $_SESSION['pagina_precedente']=$_SESSION['pagina_corrente'];
-
-    }
-
-    $_SESSION['pagina_corrente']=basename($_SERVER['REQUEST_URI']);
-
-    if(!isset($_SESSION['pagina_precedente']))
-        $_SESSION['pagina_precedente']='index.php';
 ?>
